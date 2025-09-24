41f607aef3e6f8069abe3ad943d6945fdbd17d3b
Muhammad657 <muhammadtaqiulla@gmail.com>
2025-09-23 22:15:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -195,4 +195,5 @@ def run_ecrecommender():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8000))
-    server.run(host='0.0.0.0', port=port)
+    gunicorn main:server --bind 0.0.0.0:$PORT --workers 1
+
